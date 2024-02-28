3d8640ab1083c0390f6aa6b85e760b4824c13369
Sujan STC <sujandstc@gmail.com>
2024-02-28 16:29:18 +0545

    Changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const createData = require("./crud_create");
+const createData = require("./routes/crud_create");
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.mongo_connect, {})
@@ -14,9 +15,8 @@ mongoose
     console.log("Connection failed!");
   });
 
-// Creating an schema (Table headers)
 
-app.get("/addData", createData);
+app.post("/addData", createData);
 
 // Starting a server
 app.listen(4000, () => {
---
+++ routes/crud_create.js
@@ -0,0 +1,46 @@
+const moviesModel = require("../models/movies.model");
+
+const createData = async (req, res) => {
+  // get data from user...
+  const { name, rating, image, info } = req.body;
+
+  // try catch block
+  try {
+    // If there is validation error, we throw an error which will be caught by catch block ..
+    if (!name) throw "Name is required";
+    if (name.length < 3) throw "Name must be more than 3 characters!";
+    if (!image) throw "Image is required";
+    if (!info) throw "Info is required";
+    if (!rating) throw "Rating is required";
+
+    // If there is no validation error, we will then create data on database...
+
+    await moviesModel.create({
+      rating: rating,
+      name: name,
+    });
+
+    // send success status to user...
+
+    res.status(200).json({
+      status: "success",
+      message: "Data created successfully!",
+    });
+  } catch (error) {
+    // Check type of error!
+
+    if (typeof error == "string") {
+      res.status(400).json({
+        status: "error",
+        message: error,
+      });
+    } else {
+      res.status(400).json({
+        status: "error",
+        message: "Something went wrong!",
+      });
+    }
+  }
+};
+
+module.exports = createData;
